64ca9ea8794d6b79a4e9f72fdc278fd68738ec7b
Aadrash Mehta <mehta.aadrash05@gmail.com>
2025-09-22 14:29:15 +0530

    Basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
+
+import Header from "./components/Header.jsx"
+import UserInput from "./components/UserInput.jsx"
+import Results from "./components/Results.jsx"
+
+const INITIAL_VALUES = {
+  initialInvestment: 1000,
+  annualInvestment: 1200,
+  expectedReturn: 6,
+  duration: 10
+}
+
 function App() {
+  const [userInput, setUserInput] = useState({...INITIAL_VALUES});
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevInput) => ({
+      ...prevInput,
+      [inputIdentifier]: +newValue
+    }))
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <Header />
+      <UserInput onChangeInput={handleChange} userInput={userInput}/>
+      <Results input={userInput} />
+    </>
   )
 }
 
